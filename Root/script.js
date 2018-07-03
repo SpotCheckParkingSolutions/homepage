7be4852b4299030523266db298aae6f77a39de70
cooper-runstein <cooperun@gmail.com>
2018-07-03 00:35:15 -0700

    Sub routes functional

--- Root/script.js
+++ Root/script.js
@@ -1,23 +1,114 @@
+  //Main Routes   
+    const products = { template: 
+        `<div>
+            <div>Our Products</div>
+            <router-view></router-view>
+        </div>`  };
+    const our_app = { template: '<div><div>App</div><router-view></router-view></div>' };
+    const about = { template: '<div><div>About Us</div><router-view></router-view></div>'};
+    const jobs = { template: '<div><div>Jobs</div><router-view></router-view></div>'};
+    const contact = { template: '<div><div>Contact</div>'};
+
+    //*********Sub Routes************
+    //Products
+    const vision = { template: '<div>Vision Network</div>' };
+    const data = { template: '<div>Data Services</div>' };
+    const pricing = { template: '<div>General Pricing</div>' };
+    const quota = { template: '<div>Get Quota</div>' };
+
+    //App
+    const coverage = { template: '<div>Coverage</div>' };
+    const myspotcheck = { template: '<div>My SpotCheck</div>' };
+    const map = { template: '<div>Map</div>' };
+
+    //about
+    const leadership = { template: '<div>Leadership</div>' };
+    const summary = { template: '<div>Summary</div>' };
+
+    //Jobs
+    const full = { template: 'Part Time Listings' };
+    const part = { template: 'Part Time Listings'};
+    const contract = { template: 'Contract and Specific Project Listings'};
+    const newest = { template: 'New Job Openings'};
+
     
-const products = { template: '<div>Products and Services</div>' }
-const our_app = { template: '<div>App</div>' }
-const about = { template: '<div>About Us</div>'}
-const jobs = { template: '<div>Jobs</div>'}
-const contact = { template: '<div>Contact</div>'}
-
-const routes = [
-    { path: '/products', component: products },
-    { path: '/app', component: our_app },
-    { path: '/about', component: about},
-    { path: '/jobs', component: jobs},
-    { path: '/contact', component: contact}
-]
 
 
-const router = new VueRouter({
-    routes
-})
+    const routes = [
+        { path: '/products', component: products,
+            children: [
+                {
+                    path: 'vision',
+                    component: vision
+                },
+                {
+                    path: 'data',
+                    component: data
+                },
+                {
+                    path: 'pricing',
+                    component: pricing
+                },
+                {
+                    path: 'quota',
+                    component: quota
+                }
+            ] },
+        { path: '/app', component: our_app,
+            childern: [
+                {
+                    path: 'coverage',
+                    component: coverage
+                },
+                {
+                    path: 'myspotcheck',
+                    component: myspotcheck
+                },
+                {
+                    path: 'map',
+                    component: map
+                },
+                
+            ] },
+        { path: '/about', component: about,
+            children: [
+                {
+                    path: 'summary',
+                    component: summary
+                },
+                {
+                    path: 'leadership',
+                    component: leadership
+                },
 
-const app = new Vue({
-    router
-}).$mount('#app')
+            ]},
+        { path: '/jobs', component: jobs,
+            childern:[
+                {
+                    path: 'full',
+                    component: full
+                },
+                {
+                    path: 'part',
+                    component: part
+                },
+                {
+                    path: 'contract',
+                    component: contract
+                },
+                {
+                    path: 'new',
+                    component: newest
+                }
+            ]},
+        { path: '/contact', component: contact}
+    ]
+
+
+    const router = new VueRouter({
+        routes
+    })
+
+    const app = new Vue({
+        router
+    }).$mount('#app')
